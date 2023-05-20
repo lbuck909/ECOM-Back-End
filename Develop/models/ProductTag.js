095ee1b571b4435c8d2e552c1c728c22ea6c2dd9
LaiSha Buck <laishabuck@gmail.com>
2023-05-20 19:02:34 -0400

    product ag deined

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,8 +6,17 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.STRING,
   },
+
+  
   {
     sequelize,
     timestamps: false,
